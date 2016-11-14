889ad1d5d420e4c88e96e939726ab76d65ee6a8b
e-e-e <e-e-e@users.noreply.github.com>
2016-11-14 11:25:51 +0100

    remove unnecessary code from main app - fix prepareSettings function - set default size to canon camera capture size

--- include/DoingDancingApp.h
+++ include/DoingDancingApp.h
@@ -4,29 +4,16 @@
 #define DoingDanceingApp_h
 
 #include "cinder/app/App.h"
-#include "cinder/app/RendererGl.h"
-#include "cinder/gl/gl.h"
-
-#include "cinder/Capture.h"
-#include "cinder/qtime/AvfWriter.h"
-#include "cinder/qtime/QuickTimeGl.h"
-
-#include "EDSDK.h"
-#include "EDSDKTypes.h"
-#include "EDSDKErrors.h"
 
 #include "CaptureLooper.hpp"
-
-#define EOS_USE true
-#define RECORDING_TIME 90
-#define OFF_TIME 30
-
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
 class DoingDancingApp : public App {
+    
+    CaptureLooper*          capture;
     
 public:
     
@@ -37,50 +24,6 @@ public:
     void update() override;
     void draw() override;
     void cleanup() override;
-    
-private:
-    
-    CaptureLooper*          capture;
-    Boolean                 recording = false;
-    u_int8_t                recording_count = 0;
-    u_int32_t               recording_timer = 0;
-    fs::path                saveFolder;
-    
-#if EOS_USE == true
-    EdsError err;
-    EdsCameraRef camera;
-    bool isSDKLoaded;
-#else
-    CaptureRef              mCapture;
-#endif
-    
-    qtime::MovieWriterRef   mMovieExporter;
-    qtime::MovieGlRef		mMovie;
-    
-    gl::TextureRef			mFrameTexture;
-    gl::TextureRef          mTexture;
-
-    void printDevices();
-    
-    void startVideoRecording();
-    void updateVideoRecording();
-    void stopVideoRecording();
-    void loadMovie(const fs::path &moviePath);
-    
-    fs::path getVideoRecordingPath (int);
-    
-#if EOS_USE == true
-    
-    EdsError getFirstCamera(EdsCameraRef *camera);
-    
-    static EdsError EDSCALLBACK handlePropertyEvent( EdsPropertyEvent, EdsPropertyID, EdsUInt32, EdsVoid *);
-    static EdsError EDSCALLBACK handleObjectEvent( EdsObjectEvent event, EdsBaseRef object,EdsVoid * context);
-    
-    EdsError startLiveview();
-    EdsError endLiveview();
-    EdsError downloadEvfData();
-    
-#endif
     
 };
 
